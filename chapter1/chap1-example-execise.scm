08441a11cd55f0604c9dfe72486863710800d31f
yangzhongming <yangzhongming@shijinshi.cn>
2019-12-12 17:59:27 +0800

    Exercise 1.27 complete

--- chapter1/chap1-example-execise.scm
+++ chapter1/chap1-example-execise.scm
@@ -312,4 +312,20 @@
             (cons (car lst) (up (cdr lst)))
             (append (car lst) (up (cdr lst)))))))
 
+;Exercise 1.27 [**] (flatten slist) returns a list of the symbols contained in
+;slist in the order in which they occur when slist is printed. Intuitively, flatten
+;removes all the inner parentheses from its argument.
+;flatten : Slist -> Listof(schemeVal)
+(define flatten
+  (lambda (slist)
+    (if (null? slist)
+        '()
+        (append
+         (flatten-in-sexp (car slist))
+         (flatten (cdr slist))))))
 
+(define flatten-in-sexp
+  (lambda (sexp)
+    (if (symbol? sexp)
+        (list sexp)
+        (flatten sexp))))
