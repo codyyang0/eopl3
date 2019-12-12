2e3decddcd2eeeb3e28cfe6d43f30f89d8cb924f
yangzhongming <yangzhongming@shijinshi.cn>
2019-12-12 17:34:53 +0800

    exercise 1.26 complete

--- chapter1/chap1-example-execise.scm
+++ chapter1/chap1-example-execise.scm
@@ -189,27 +189,127 @@
 
 ;Exercise 1.18 [*] (swapper s1 s2 slist) returns a list the same as slist, but
 ;with all occurrences of s1 replaced by s2 and all occurrences of s2 replaced by s1.
-;swapper : Sym * Sym * Listof(schemeval) -> Listof(schemeval)
+;swapper : Sym * Sym * slist -> slist
 (define swapper
   (lambda (s1 s2 slist)
+    (if (null? slist)
+        '()
+        (cons
+         (swapper-elements-in-sexp s1 s2 (car slist))
+         (swapper s1 s2 (cdr slist))))))
+
+(define swapper-elements-in-sexp
+  (lambda (s1 s2 sexp)
+    (if (symbol? sexp)
+        (if (eqv? s1 sexp) s2
+            (if (eqv? s2 sexp) s1 sexp))
+        (swapper s1 s2 sexp))))
+
+
+;Exercise 1.19 [**](list-set lst n x) returns a list like lst, except that the n-th
+;element, using zero-based indexing, is x.
+;list-set : Listof(Sym) * Int * SchemeVal -> Listof(SchemeVal)
+(define list-set
+  (lambda (lst n x)
     (cond
-      ((null? slist) '())
-      ((symbol? (car slist))
-       (cond
-         ((eqv? s1 (car slist)) (cons s2 (swapper s1 s2 (cdr slist))))
-         ((eqv? s2 (car slist)) (cons s1 (swapper s1 s2 (cdr slist))))
-         (else
-          (cons (car slist) (swapper s1 s2 (cdr slist))))))
-      (cons
-       (swapper s1 s2 (car slist))
-       (swapper s1 s2 (cdr slist))))))
+      ((null? lst) '())
+      ((zero? n) (cons x (cdr lst)))
+      (else
+       (cons (car lst)
+             (list-set (cdr lst) (- n 1) x))))))
+
+;Exercise 1.20 [*] (count-occurrences s slist) returns the number of
+;occurrences of s in slist
+;count-occurrences : Sym * Slist -> Int
+(define count-occurrences
+  (lambda (s slist)
+    (if (null? slist)
+        0
+        (+ (count-occurrences-s-in-sexp s (car slist))
+           (count-occurrences s (cdr slist))))))
+
+(define count-occurrences-s-in-sexp
+  (lambda (s sexp)
+    (if (symbol? sexp)
+        (if (eqv? s sexp) 1 0)
+        (count-occurrences s sexp))))
+
+;Exercise 1.21 [**] (product sos1 sos2), where sos1 and sos2 are each a list
+;of symbols without repetitions, returns a list of 2-lists that represents the
+;Cartesian product of sos1 and sos2. The 2-lists may appear in any order.
+;product : Listof(Sym) * Listof(Sym) -> Listof(Sym)
+(define product
+  (lambda (los1 los2)
+    (if (null? los1)
+        '()
+        (append
+         (product-s-los (car los1) los2)
+         (product (cdr los1) los2)))))
+
+(define product-s-los
+  (lambda (s los)
+    (if (null? los)
+        '()
+        (cons
+         (list s (car los))
+         (product-s-los s (cdr los))))))
+
+;Exercise 1.22 [**] (filter-in pred lst) returns the list of thoese elements in
+;lst that satisfy the predicate pred.
+;filter-in : pred * Listof(schemeVal) -> Listof(schemeVal)
+(define filter-in
+  (lambda (pred lst)
+    (if (null? lst)
+        '()
+        (if (pred (car lst))
+            (cons (car lst) (filter-in pred (cdr lst)))
+            (filter-in pred (cdr lst))))))
         
+;Exercise 1.23 [**] (list-index pred lst) returns the 0-based position of the
+;first element of lst that satisfies the predicate pred. If no element of lst satisfies
+;the predicate, then list-index returns #f.
+;list-index : pred * Listof(schemeVal) -> Int
+(define list-index
+  (lambda (pred lst)
+    (if (null? lst)
+        0
+        (if (pred (car lst))
+            0
+            (+ 1 (list-index pred (cdr lst)))))))
 
+;Exercise 1.24 [**] (every? pred lst) returns #f if any element of lst fails to
+;satisfy pred, and returns #t otherwise.
+;every? : pred * Listof(symbol) -> boolean
+(define every?
+  (lambda (pred lst)
+    (if (null? lst)
+        #t
+        (if (pred (car lst))
+            (every? pred (cdr lst))
+            #f))))
+            
+;Exercise 1.25 [**] (exists? pred lst) returns #t if any element of lst satisfies
+;pred, and returns #f otherwise.
+;exists? : pred * Listof(symbol) -> boolean
+(define exists?
+  (lambda (pred lst)
+    (if (null? lst)
+        #f
+        (if (pred lst)
+            #t
+            (exists? pred (cdr lst))))))
 
-  
-
-
-  
-
+;Exercise 1.26 [**] (up lst) removes a pair of parentheses from each top-level element
+;of lst.If a top-level element is not a list, it is included in the result, as is.
+;The value of (up (down lst)) is equivalent to lst, but (down (up lst) is not neccessarily
+;lst.(See exercise 1.17.)
+;up : ListOf(schemeVal) -> ListOf(schemeVal)
+(define up
+  (lambda (lst)
+    (if (null? lst)
+        '()
+        (if (not (pair? (car lst)))
+            (cons (car lst) (up (cdr lst)))
+            (append (car lst) (up (cdr lst)))))))
 
 
